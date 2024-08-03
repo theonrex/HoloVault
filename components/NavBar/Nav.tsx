b5f2d4c253f1f1541fc3e7165eb9a9a96a700e29
Olawande Armstrong <91835162+theonrex@users.noreply.github.com>
2024-08-03 14:01:56 +0100

    Update Nav.tsx

--- components/NavBar/Nav.tsx
+++ components/NavBar/Nav.tsx
@@ -8,7 +8,7 @@ import styles from "../SolWallet/ConnectWallet.module.css"
 export function NavBody() {
   return (
     <Navbar fluid rounded className="bg-black">
-      <Navbar.Brand href="https://flowbite-react.com">
+      <Navbar.Brand href="/">
         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
           HoloVault
         </span>
